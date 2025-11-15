92128d5aaa2ba3b84399eeb886455a9045d120be
ben <172694952+galacticmice@users.noreply.github.com>
2025-11-15 15:22:40 -0800

    sharding planning stage

---
+++ db/router.py
@@ -0,0 +1,3 @@
+import sqlite3, json, os, threading
+
+config = json.load(open("node_config.json"))
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from distributed-database-wrapper!")
+
+
+if __name__ == "__main__":
+    main()
